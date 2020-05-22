ba97383435d83c8fbd4f25b3050402db27a705c2
st <63641764+emnast@users.noreply.github.com>
2020-05-22 11:20:58 +0100

    Update BatchJobExecution.java

--- src/main/java/com/example/demo/entity/BatchJobExecution.java
+++ src/main/java/com/example/demo/entity/BatchJobExecution.java
@@ -1,4 +1,4 @@
-package com.example.demo.entity;
+/*package com.example.demo.entity;
 
 
 import java.util.Date;
@@ -137,7 +137,7 @@ public class BatchJobExecution {
 	}
     
 
-}
+}*/
 
 /*	 public Integer getJobExecutionId() {
 	        return jobExecutionId;
